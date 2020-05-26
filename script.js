6092cc9c8f48cdc5a9bc035655a8794b307215a8
Himanshu sharma <himanshush214@gmail.com>
2020-05-26 19:57:19 +0530

    Added Pagination

--- script.js
+++ script.js
@@ -57,41 +57,238 @@ const result = [
     image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
     rating:"4.9"
   },
+  {
+    title:"Kalaam Restraunt",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  },
+  {
+    title:"Crazy About Pizza",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  },
+  {
+    title:"Highway Kitechen",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4.5"
+  },
+  {
+    title:"Big Bite",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  },
+  {
+    title:"XCF Restra",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"3.5"
+  },
+  {
+    title:"Biaknerwala",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  },
+  {
+    title:"Kung fu panda",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  } , {
+    title:"Big Lite",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"1"
+  },
+    {
+    title:"Mayank Dhaba",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  } , {
+    title:"AAAA Resort",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4.9"
+  },
+  {
+    title:"Kalaam Restraunt",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  },
+  {
+    title:"Crazy About Pizza",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  },
+  {
+    title:"Highway Kitechen",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4.5"
+  },
+  {
+    title:"Big Bite",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  },
+  {
+    title:"XCF Restra",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"3.5"
+  },
+  {
+    title:"Biaknerwala",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  },
+  {
+    title:"Kung fu panda",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  } , {
+    title:"Big Lite",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"1"
+  },
+    {
+    title:"Mayank Dhaba",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  } , {
+    title:"AAAA Resort",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4.9"
+  },
+  {
+    title:"Kalaam Restraunt",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  },
+  {
+    title:"Crazy About Pizza",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  },
+  {
+    title:"Highway Kitechen",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4.5"
+  },
+  {
+    title:"Big Bite",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  },
+  {
+    title:"XCF Restra",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"3.5"
+  },
+  {
+    title:"Biaknerwala",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  },
+  {
+    title:"Kung fu panda",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4"
+  } , {
+    title:"Big Lite",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"1"
+  },
+    {
+    title:"Mayank Dhaba",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"5"
+  } , {
+    title:"AAAA Resort",
+    description:"Lorem ipsum dolor, sit amet contesectetur adipisicing elit. Ipsam ab omnis illo inventore earum explicabo cupiditate impedit quaerat laudantium ipsa doloremque quae iure laborum assumenda quia, dolore mollitia vitae iusto.",
+    image_link:"https://media.istockphoto.com/photos/interior-of-cozy-restaurant-loft-style-picture-id843610508?k=6&m=843610508&s=612x612&w=0&h=41DLQdNj3YtjIbSUQtX2zEvdJHAdHAq5c1MVS37VNzo=",
+    rating:"4.9"
+  }
 ]
-
+var currentPage = 1;
+var numPages = 0;
+var restraPerPage = 8;
+var index;
+var prev=0;
+const root = document.querySelector(".root");
 const container = document.querySelector(".container");
 const searchbar = document.getElementById("searchbar");
 const filterS = document.getElementById("filterS");
+
+
 searchbar.addEventListener("keyup",(e)=>{
   const tempRes = result.filter((item)=>item.title.toLowerCase().includes(e.target.value.toLowerCase()));
-  init(tempRes);
+  init(1,tempRes);
 });
 filterS.addEventListener("change",(e)=>{
   let temp = [...result];
   if(e.target.value==="SortByRating"){ 
     temp.sort((a,b) => Number(a.rating) > Number(b.rating) ? -1 : 1);
-      init(temp);
+      init(1,temp);
   }
   else if(e.target.value==="SortByTitle"){
     temp.sort((a,b)=>a.title>b.title?1:-1);
-      init(temp);
+      init(1,temp);
   }
     else if(e.target.value==="default"){
-    init(result);
+    init(1,result);
   }
 
 });
 window.onload = function(){
-  init(result);
+  // console.log(numberOfRestra());
+  // console.log(numberOfPages())
+  root.appendChild(paginationFilter());
+  createPages();
+  init(1,result);
 }
-function init(res){
+function init(n,res){
+  index  = n*restraPerPage;
+  prev = index-restraPerPage;
+  temp = res.slice(prev,index);
   let text = '';
-  res.forEach((item)=>{ 
+  temp.forEach((item)=>{ 
     text+=display(item);
   })
   
   container.innerHTML = text;
 }
+// function filter(res){
+  
+//   let text = '';
+//   res.forEach((item)=>{ 
+//     text+=display(item);
+//   })
+  
+//   container.innerHTML = text;
+// }
 function display(item){
   let html = `<div class="card">
     <div class="card_image">
@@ -117,4 +314,65 @@ function display(item){
     </div>
   </div>`;
   return html;
+}
+var numberOfRestra = function () {
+	var numberOfRestra = result.length;
+	return (numberOfRestra);
+}
+
+//Finding the number of pages
+var numberOfPages = function () {
+
+	var numberOfPages = parseInt(numberOfRestra() / restraPerPage);
+	
+	if ( numberOfRestra() % restraPerPage > 0 ){
+		numPages += 1;
+	}
+	
+	return numberOfPages;
+};
+
+function createPages () {
+
+	for (var i = 0; i < numberOfPages(); i++) {
+		//Create the dynamic elements in the pagination
+		var liList = document.createElement('li');
+		var pageLink = document.createElement('a');
+		//Set attributes to the pagination links & the li list 
+		pageLink.setAttribute("href", "#");
+		pageLink.setAttribute("class", "link");
+		liList.setAttribute("class", "pageLi");
+		//Append the different elements to the static ulList
+		var pagelist = document.getElementById("pagelist");
+		pagelist.appendChild(liList);
+		liList.appendChild(pageLink);
+		//Create and appending the page numbers for the pagination
+		var pageNumbers = document.createTextNode(i + 1);
+		pageLink.appendChild(pageNumbers);
+
+		pageLink.addEventListener("click", function(){
+	   		var pageNumClicked = (this.innerHTML || this.innerText);
+	   		changePage(pageNumClicked);
+		}, false);
+
+
+	}
+	return i;
+};
+
+var paginationFilter = function() {
+	//Create the static elements for the pagination 
+	var pagination = document.createElement('div');
+	var ulList = document.createElement('ul');
+	//Giving them attributes to select them easily	
+	pagination.setAttribute("class", "pagination");
+	ulList.setAttribute("id", "pagelist");
+	//Append the ulList to the main pagination div
+	pagination.appendChild(ulList);
+	//Return the main div that contains all the elements
+	return pagination;	
+};
+
+function changePage(pageNumClicked){
+  init(pageNumClicked,result);
 }
